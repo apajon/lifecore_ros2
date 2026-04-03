cec4c7db5da7c7f7b5c188fdceef3a37b7df9381
Adrien Pajon <7260230+apajon@users.noreply.github.com>
2026-04-03 14:40:09 -0400

    chore(version): refresh scm version metadata

--- src/lifecore_ros2/_version.py
+++ src/lifecore_ros2/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = "0.1.dev3+gccb743e1a.d20260402"
-__version_tuple__ = version_tuple = (0, 1, "dev3", "gccb743e1a.d20260402")
+__version__ = version = "0.0.2.dev0+g3b17c0044.d20260403"
+__version_tuple__ = version_tuple = (0, 0, 2, "dev0", "g3b17c0044.d20260403")
 
-__commit_id__ = commit_id = "gccb743e1a"
+__commit_id__ = commit_id = "g3b17c0044"
