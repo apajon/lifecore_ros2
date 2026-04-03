dbe00025fdc5724930cac27d3e90c4e2b750a892
Adrien Pajon <7260230+apajon@users.noreply.github.com>
2026-04-03 14:45:05 -0400

    fix(version): align generated version file with v0.1.0 tag

--- src/lifecore_ros2/_version.py
+++ src/lifecore_ros2/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = "0.0.2.dev0+g3b17c0044.d20260403"
-__version_tuple__ = version_tuple = (0, 0, 2, "dev0", "g3b17c0044.d20260403")
+__version__ = version = "0.1.0"
+__version_tuple__ = version_tuple = (0, 1, 0)
 
-__commit_id__ = commit_id = "g3b17c0044"
+__commit_id__ = commit_id = "g9cf6953"
